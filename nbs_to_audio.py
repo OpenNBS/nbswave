fbaefb5844db08a4baf4251df5a8081ba7a38ee3
Bentroen <bemcdc@gmail.com>
2021-01-29 20:55:42 -0300

    Use better algorithm to avoid unnecessary pitch/gain changes

--- nbs_to_audio.py
+++ nbs_to_audio.py
@@ -2,9 +2,12 @@ import os
 import pydub
 import pynbs
 import math
+from collections import namedtuple
 
 
 SOUNDS_PATH = "sounds"
+
+Note = namedtuple('Note', 'tick layer instrument pitch volume panning')
 
 
 instruments = [
@@ -75,6 +78,14 @@ def get_panning(note, layer):
 	return pan
 
 
+def key_to_pitch(key):
+	return 2**((key) / 12)
+
+
+def vol_to_gain(vol):
+	return math.log(max(vol, 0.0001), 10) * 20
+
+
 def sort_notes(song):
 
 	notes = []
@@ -85,10 +96,10 @@ def sort_notes(song):
 		volume = get_volume(note, layer)
 		panning = get_panning(note, layer)
 		
-		new = Note(note.tick, note.instrument, pitch, volume, panning)
+		new = Note(note.tick, note.layer, note.instrument, pitch, volume, panning)
 		notes.append(new)
 	
-	return sorted(notes, key=lambda x: (x.instrument, x.pitch, x.volume, x.panning))
+	return sorted(notes, key=lambda x: (x.pitch, x.instrument, x.volume, x.panning))
 	
 
 def render_audio(song, output_path, loops=0, fadeout=False, target_bitrate=320, target_size=None):
@@ -99,39 +110,66 @@ def render_audio(song, output_path, loops=0, fadeout=False, target_bitrate=320, 
 	track = pydub.AudioSegment.silent(duration=length)
 	master_gain = -12.0
 	
-	for i, note in enumerate(song.notes):
+	last_ins = None
+	last_key = None
+	last_vol = None
+	last_pan = None
+	
+	ins_changes = 0
+	key_changes = 0
+	vol_changes = 0
+	pan_changes = 0
+	
+	sorted_notes = sort_notes(song)
+	for i, note in enumerate(sorted_notes):
 		
 		if note.instrument > song.header.default_instruments - 1:
 			continue
 		
-		sound = instruments[note.instrument]
+		ins = note.instrument
+		key = note.pitch
+		vol = note.volume
+		pan = note.panning
 		
-		key = note.key - 45
-		detune = note.pitch / 100
-		pitch = 2**((key + detune) / 12)
-		pos = note.tick / song.header.tempo * 1000
+		#Todo: optimize and avoid gain/pitch/key calculation if default value!
+		#Todo: ignore locked layers
+		#Todo: pan has a loudness compensation? https://github.com/jiaaro/pydub/blob/master/API.markdown#audiosegmentpan
 		
-		layer_vol = song.layers[note.layer].volume / 100
-		note_vol = note.velocity / 100
-		vol = layer_vol * note_vol
-		if vol == 0:
-			continue
-		gain = math.log(max(vol, 0.0001), 10) * 20
+		if ins != last_ins:
+			last_key = None
+			last_vol = None
+			last_pan = None
+			sound1 = instruments[note.instrument]
+			sound1 = sound1.apply_gain(master_gain)
+			ins_changes += 1
+			
+		if key != last_key:
+			last_vol = None
+			last_pan = None
+			pitch = key_to_pitch(key)
+			sound2 = change_speed(sound1, pitch)
+			key_changes += 1
+			
+		if vol != last_vol:
+			last_pan = None
+			gain = vol_to_gain(vol)
+			sound3 = sound2.apply_gain(gain)
+			vol_changes += 1
+			
+		if pan != last_pan:
+			sound4 = sound3.pan(pan)
+			sound = sound4
+			pan_changes += 1
 		
-		layer_pan = -song.layers[note.layer].panning / 100
-		note_pan = note.panning / 100
-		if layer_pan == 0:
-			pan = note_pan
-		else:
-			pan = (layer_pan + note_pan) / 2
-		pan = min(max(pan, -1), 1)
+		last_ins = ins
+		last_key = key
+		last_vol = vol
+		last_pan = pan
 		
 		if i % 10 == 0:
 			print("Converting note {}/{} (tick: {}, layer: {}, vol: {}, pan: {}, pit: {})".format(i+1, len(song.notes), note.tick, note.layer, vol, pan, pitch))
 		
-		sound = sound.apply_gain(gain).pan(pan)
-		sound = sound.apply_gain(master_gain)
-		sound = change_speed(sound, pitch)
+		pos = note.tick / song.header.tempo * 1000
 		
 		# Ensure track is long enough to hold the note
 		diff = (pos + len(sound)) - len(track)
@@ -157,3 +195,4 @@ def render_audio(song, output_path, loops=0, fadeout=False, target_bitrate=320, 
 							   tags={"artist": "test"})
 	
 	return file_handle
+	
