8b978f09f953d74885631ac8b47d02e0a038a392
Bentroen <bemcdc@gmail.com>
2022-03-05 06:02:05 -0300

    Remove hardcoded path from test module

--- test.py
+++ test.py
@@ -1,17 +1,12 @@
-import os
 import time
+from pathlib import Path
 
-from nbsaudio.main import render_audio
+from nbswave import render_audio
 
 song = "test.nbs"
-
 output = "test.mp3"
 
-custom_sound_path = os.path.join(
-    os.environ["USERPROFILE"], "Minecraft Note Block Studio", "Data", "Sounds"
-)
-custom_sound_path = custom_sound_path.replace("C:", "D:")
-print(custom_sound_path)
+custom_sound_path = Path.home() / "Minecraft Note Block Studio" / "Data" / "Sounds"
 
 start = time.time()
 render_audio(song, output, custom_sound_path=custom_sound_path, format="mp3")
