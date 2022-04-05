Fix pop in exported track due to wrong peak calculation
This difference of one unit would result in the peak being calculated as smaller than it is, so the applied attenuation would be slightly less than necessary, causing the peak value to overflow during the conversion to `int16` and leading to a pop in the exported track.

nbswave/audio.py
@@ -102,7 +102,7 @@ class Mixer:
 
     def to_audio_segment(self):
         peak = np.abs(self.output).max()
-        clipping_factor = peak / (2 ** 15)
+        clipping_factor = peak / (2 ** 15 - 1)
 
         if clipping_factor > 1:
             print(